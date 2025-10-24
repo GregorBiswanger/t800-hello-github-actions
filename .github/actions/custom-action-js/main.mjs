a491e49ca7f6733703025c753257ef860c95d8c4
Gregor Biswanger <gregor.biswanger@web-enliven.de>
2025-10-24 07:38:41 -0600

    add custom github action

---
+++ .github/actions/custom-action-js/main.mjs
@@ -0,0 +1,11 @@
+import core from '@actions/core';
+
+try {
+    const name = core.getInput('name');
+    console.log(`Hello ${name}!`);
+    
+    const time = new Date().toTimeString();
+    core.setOutput('time', time);
+} catch (error) {
+    core.setFailed(error.message);
+}
